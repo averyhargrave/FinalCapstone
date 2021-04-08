5c348783870af2c8beef8e4a83f2616ea8f0f2e3
David Buckley <dabjr0@gmail.com>
2021-04-08 14:08:34 -0400

    push

--- final-capstone/front-end/vue/src/services/ItineraryServices.js
+++ final-capstone/front-end/vue/src/services/ItineraryServices.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 export default {
 
 createItinerary() {
-    return axios.post(`/create/itinerary`);
+    return axios.post(`/createItinerary`);
 },
 
 getItineraryById(userId) {
-    return axios.get(`/itineraryByUser/${userId}`);
+    return axios.get(`/itineraryUser/${userId}`);
 },
 
 deleteItinerary(itineraryId) {
